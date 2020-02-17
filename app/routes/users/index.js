825589502d554b3bdb6e67ac16ed9ea4bb3486ae
Alexander Lomonosov <skilldill@yandex.ru>
2020-02-17 23:35:44 +0500

    update

--- app/routes/users/index.js
+++ app/routes/users/index.js
@@ -1,12 +1,12 @@
 export default function UsersRoute(app, path, config) {
     // work with one url, methods: GET, POST
     app.get(path, (req, res) => {
-        res.send({ status: "ok" });
+        res.json({ status: "ok" });
     });
 
     app.post(path, (req, res) => {
         const user = req.body;
         console.log("success add user");
-        res.send({ status: "added", user });
+        res.json({ status: "added", user });
     });
 }
